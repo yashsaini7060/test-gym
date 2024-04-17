a066dbaad5b18bc1b4a7b1ae879802cd113f9164
yashsaini7060 <yk18052000@gmail.com>
2024-04-17 16:31:57 +0530

    height and width added

---
+++ index.js
@@ -0,0 +1,15 @@
+const browserWidth = window.innerWidth;
+const browserHeight = window.innerHeight;
+
+
+const heighDiv = document.getElementById("height");
+const widthDiv = document.getElementById("width");
+
+heighDiv.innerText=browserHeight;
+
+widthDiv.innerText=browserWidth;
+
+console.log(`Browser width: ${browserWidth}px`); 
+console.log(`Browser height: ${browserHeight}px`); 
+
+
